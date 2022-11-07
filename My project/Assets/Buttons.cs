8caa8334fd872126f26c5ab8d3d7e63115eb5a1e
Alexandra Ion <alexion200123@gmail.com>
2022-11-08 00:55:44 +0200

    meniu_labirint

---
+++ My project/Assets/Buttons.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void ExitFame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
