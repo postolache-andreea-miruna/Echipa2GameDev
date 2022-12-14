d22653c1d3a10de9ad1342f2b27f2b05d9d1afb1
Postolache Andreea Miruna <79594745+postolache-andreea-miruna@users.noreply.github.com>
2022-12-14 23:49:14 +0200

    Strawberry logic

--- My project/Assets/Scripts/Buttons.cs
+++ My project/Assets/Scripts/Buttons.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEditor;
 
 public class Buttons : MonoBehaviour
 {
+
+    //public CollectItems stro;
+    private int strawberry;
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -32,5 +36,67 @@ public class Buttons : MonoBehaviour
     public void RestartGameWires()
     {
         SceneManager.LoadScene("BridgeMeniu");
+    }
+    public void StrawberryWire()
+    {
+        strawberry = PlayerPrefs.GetInt("Coins", 0);
+        // Debug.Log(strawberry);
+        if (strawberry >= 17)
+        {
+            strawberry -= 17;
+            SceneManager.LoadScene("BridgeWinGame");
+            PlayerPrefs.SetInt("Coins", strawberry);
+        }
+        else
+        {
+            bool popup = EditorUtility.DisplayDialog("Message", "Not enough strawberries", "OK");
+        }
+    }
+
+    public void StrawberryFishing()
+    {
+        strawberry = PlayerPrefs.GetInt("Coins", 0);
+        // Debug.Log(strawberry);
+        if (strawberry >= 15)
+        {
+            strawberry -= 15;
+            SceneManager.LoadScene("FishingWinCase");
+            PlayerPrefs.SetInt("Coins", strawberry);
+        }
+        else
+        {
+            bool popup = EditorUtility.DisplayDialog("Message", "Not enough strawberries", "OK");
+        }
+    }
+    public void StrawberryBoat()
+    {
+        strawberry = PlayerPrefs.GetInt("Coins", 0);
+       // Debug.Log(strawberry);
+        if (strawberry >= 15)
+        {
+            strawberry -= 15;
+            SceneManager.LoadScene("RiverWinGame");
+            PlayerPrefs.SetInt("Coins", strawberry);
+        }
+        else
+        {
+            bool popup = EditorUtility.DisplayDialog("Message", "Not enough strawberries", "OK");
+        }
+    }
+
+    public void StrawberryMaze()
+    {
+        strawberry = PlayerPrefs.GetInt("Coins", 0);
+       // Debug.Log(strawberry);
+        if (strawberry >= 10)
+        {
+            strawberry -= 10;
+            SceneManager.LoadScene("MazeWinGame");
+            PlayerPrefs.SetInt("Coins", strawberry);
+        }
+        else
+        {
+            bool popup = EditorUtility.DisplayDialog("Message", "Not enough strawberries", "OK");
+        }
     }
 }
